8b7b5ab6abd023f414d5196f4136c8a705542a98
LetYana <114302100+LetYana@users.noreply.github.com>
2022-11-01 13:48:50 +0100

    Week 5 results

---
+++ js/js.js
@@ -0,0 +1,94 @@
+// Current time
+let now = new Date();
+
+let actualDate = document.querySelector(".actual-date");
+let actualTime = document.querySelector(".actual-time");
+
+let dayOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = dayOfTheWeek[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Augest",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let year = now.getFullYear();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+actualDate.innerHTML = `${month} ${date}, ${year}`;
+actualTime.innerHTML = `${day} ${hours}:${minutes}`;
+
+// City choice
+function displayWeatherConditions(response) {
+  console.log(response.data);
+  document.querySelector(".actual-city").innerHTML = response.data.name;
+  document.querySelector("#main-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  let currentWeatherDescription = response.data.weather[0].description;
+  document.querySelector(".general-weather-description").innerHTML =
+    currentWeatherDescription.charAt(0).toUpperCase() +
+    currentWeatherDescription.slice(1);
+}
+
+function searchCity(city) {
+  let apiKey = "1da1773b8d8c37236ad8e28c3090a15f";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+function handleSubmit(event) {
+  debugger;
+  event.preventDefault();
+  let city = document.querySelector(".input-city").value;
+  searchCity(city);
+}
+
+let signUpForm = document.querySelector("#input-city-choice");
+signUpForm.addEventListener("submit", handleSubmit);
+
+searchCity("Amsterdam");
+
+//Geolocation API
+
+function showCurrentLocation(position) {
+  let apiKey = "1da1773b8d8c37236ad8e28c3090a15f";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showCurrentLocation);
+}
+
+let currentButton = document.querySelector("#my-location");
+currentButton.addEventListener("click", getCurrentLocation);
